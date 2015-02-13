41a569df327b176b8ad2fcd1d3d95e9931f64193
badroum <julien.dufee@gmail.com>
2015-02-13 10:40:40 +0100

    initial commit

---
+++ ical.php
@@ -0,0 +1,19 @@
+<?php
+// Log les accès pour voir la charge générée
+$log = fopen('ical_access.log', 'a');
+fwrite($log, date(DATE_RFC822).' \\\\ '.$_SERVER['REMOTE_ADDR'].' \\\\ '.$_SERVER['HTTP_USER_AGENT']."\r\n");
+fclose($log);
+
+require('function.php');
+$i=0;
+while(isset($_GET['resource' . $i]))
+{
+    $ical = $ical . GetCal(htmlspecialchars($_GET['username']),htmlspecialchars($_GET['password']),htmlspecialchars($_GET['resource'. $i]));
+    $i++;
+}
+
+str_replace("/(END:VCALENDA.*ORIAN)/s",'',$ical);
+header('Content-Type: text/calendar;charset=UTF-8');
+header("Content-disposition: attachment; filename=\"ENSIBS.ics\"");
+echo $ical;
+?>
---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+//*
+if(isset($_POST['username'])&&isset($_POST['password']))
+{
+    $generate=true;
+    if( empty ($_SERVER['REQUEST_URI']))
+    {
+      $ical="ical.php";
+    }
+    else
+    {
+        $ical= str_replace("index.php","ical.php",$_SERVER['REQUEST_URI']);
+    }
+    $tmp_Array = $_POST['resources'];
+
+    foreach ($tmp_Array as $index => $tmp)
+    {
+        $multipl_result = $multipl_result. "&resource" . $index . "=" . $tmp;
+    }
+
+
+
+
+
+    $url = dirname($_SERVER['SERVER_PROTOCOL']) . "://" . $_SERVER['HTTP_HOST'].$ical . "?username=" . $_POST['username'] ."&password=" .$_POST['password'] . $multipl_result;
+
+
+}
+//*/
+?>
+
+<!DOCTYPE html>
+<!--[if lt IE 7 ]> <html lang="fr" class="no-js ie6 lt8"> <![endif]-->
+<!--[if IE 7 ]>    <html lang="fr" class="no-js ie7 lt8"> <![endif]-->
+<!--[if IE 8 ]>    <html lang="fr" class="no-js ie8 lt8"> <![endif]-->
+<!--[if IE 9 ]>    <html lang="fr" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="fr" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="UTF-8" />
+        <!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  -->
+        <title>Login and Registration Form with HTML5 and CSS3</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Login and Registration Form with HTML5 and CSS3" />
+        <meta name="keywords" content="ensibs,ical,calendar,ubs" />
+        <meta name="author" content="Dufee-j" />
+        <link rel="shortcut icon" href="../favicon.ico">
+        <link rel="stylesheet" type="text/css" href="css/demo.css" />
+        <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+    </head>
+    <body>
+        <div class="container">
+            <!-- Codrops top bar -->
+            <header>
+                <h1>Génération d'une adresse de Calendrier</h1>
+			</header>
+            <section>
+                <div id="container_demo" >
+                    <div id="wrapper">
+                        <div id="login" class="animate form">
+
+                        <?php
+                            if(isset($generate)&& $generate==true)
+                            {
+                                echo "<span class=\"url\">" . $url . "</span>";
+                            }
+
+                            else
+                            {
+                            ?>
+                            <form  action="#" autocomplete="on" method="post">
+                                <h1>Information</h1>
+                                <p>
+                                    <label for="username" class="uname info" data-icon="u" > Votre identifiant ubs <span>Rentrez votre identifiant ubs sous la forme e1234567</span> </label>
+                                    <input id="username" name="username" required="required" type="text" placeholder="identifiant ubs"/>
+                                </p>
+                                <p>
+                                    <label for="password" class="youpasswd  info" data-icon="p"> Votre mots de passe <span>Rentrez votre mots de passe ubs</span></label>
+                                    <input id="password" name="password" required="required" type="password" placeholder="eg. X8df!90EO" />
+                                </p>
+                                <p class="resource">
+									<label for="resources[]" class="youresource info"> Votre / Vos formation <span>Sélectionner votre ou vos formation. C'est un champ à sélection multiple, maintenez Ctrl pour sélectionné plusieurs champs</span></label>
+								<SELECT NAME="resources[]" MULTIPLE SIZE=6 required="required">
+								<?php
+								require('function.php');
+								$resources = SetResources();
+								foreach ($resources as $index => $tmp)
+                                {
+                                	echo "<OPTION VALUE=" . $tmp . ">" . $index . "</option><br />";
+                                }
+
+                                ?>
+                                </SELECT>
+
+
+								</p>
+
+
+
+								<p class="login button">
+                                    <input type="submit" value="Generated" />
+								</p>
+                            </form>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </body>
+</html>
